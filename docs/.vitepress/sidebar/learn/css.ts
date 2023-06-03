46e1c369009384b0f85b9c2bb6cd091b3fcbd67f
T-miracle <1720570275@qq.com>
2023-06-03 23:11:46 +0800

    add：添加css模块文章

--- docs/.vitepress/sidebar/learn/css.ts
+++ docs/.vitepress/sidebar/learn/css.ts
@@ -4,6 +4,7 @@ const cssItems = [
     { text: '🔸 谈谈你对BFC的理解', link: '/learn/web/css/what_is_bfc' },
     { text: '🔹 drop-shadow属性的使用', link: '/learn/web/css/the_use_of_drop_shadow' },
     { text: '🔹 修改滚动条样式', link: '/learn/web/css/modify_scrollbar_style' },
+    { text: '🔹 文本多行自动省略号', link: '/learn/web/css/multiline_text_add_ellipsis' },
     { text: '🔹 font-synthesis属性导致中文斜体不生效的问题', link: '/learn/web/css/font-synthesis_property' },
 ]
 
---
+++ docs/learn/web/css/multiline_text_add_ellipsis.md
@@ -0,0 +1,16 @@
+# 文本多行自动省略号
+
+```css
+.box {
+    /* 将元素设置为超出隐藏 */
+    overflow: hidden;
+    /* 文本设置为省略号 */
+    text-overflow: ellipsis;
+    /* 定义第几行省略号 */
+    -webkit-line-clamp: 2;
+    /* 需要设置高度 */
+    height: 48px;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+}
+```
