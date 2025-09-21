c41e4435d46a744b66074e8e7c5c88f886dfd0ff
Tmiracle <1720570275@qq.com>
2025-09-21 18:24:47 +0800

    Update: 新文章

--- .vitepress/sidebar/bugs.ts
+++ .vitepress/sidebar/bugs.ts
@@ -1,15 +1,13 @@
 import {
     androidStudioIcon,
-    bugIcon, cssIcon,
+    bugIcon,
     editorIcon,
-    elementIcon,
-    htmlIcon, tauriIcon,
-    tsIcon,
-    uniAppIcon,
     webIcon
 } from './icons';
 import { type SidebarType } from '../../package/type';
-import androidStudioItems from './learn/android-studio';
+import androidStudioItems from './bugs/android-studio';
+import windowsItems from './bugs/windows';
+import web from './bugs/web';
 
 const bugsSidebar: SidebarType[] = [
     {
@@ -22,100 +20,7 @@ const bugsSidebar: SidebarType[] = [
         icon: webIcon,
         text: '前端',
         collapsed: false,
-        items: [
-            {
-                icon: htmlIcon,
-                text: 'HTML',
-                collapsed: true,
-                items: [
-                    {
-                        icon: '🔹',
-                        text: 'img 标签加载对象存储图片后再使用 canvas 调用当前图片，导致图片跨域的问题',
-                        link: '/bugs/web/html/canvas-use-object-storage-image-cross-domain'
-                    }
-                ]
-            },
-            {
-                icon: cssIcon,
-                text: 'CSS',
-                collapsed: true,
-                items: [
-                    {
-                        icon: '🔹',
-                        text: 'UnoCSS 相关错误记录',
-                        link: '/bugs/web/css/unocss_error_records'
-                    }
-                ]
-            },
-            {
-                icon: tsIcon,
-                text: 'TypeScript',
-                collapsed: true,
-                items: [
-                    {
-                        icon: '🔹',
-                        text: '代码警告：TS2307: Cannot find module \'xxx\' or its corresponding type declarations.',
-                        link: '/bugs/web/typescript/warning_ts2307'
-                    }
-                ]
-            },
-            {
-                icon: elementIcon,
-                text: 'Element-UI & Element-Plus',
-                collapsed: true,
-                items: [
-                    {
-                        icon: '🔹',
-                        text: 'el-upload 为啥没法拖拽了？',
-                        link: '/bugs/web/element-ui/el-upload-cant-drag'
-                    },
-                    {
-                        icon: '🔹',
-                        text: 'MessageBox报错Uncaught (in promise) cancel',
-                        link: '/bugs/web/element-ui/messagebox_report_error_uncaught_(in_promise)_cancel'
-                    },
-                    {
-                        icon: '🔹',
-                        text: 'Error: for nested data item, row-key is required.',
-                        link: '/bugs/web/element-ui/table_tree_json_error'
-                    },
-                    {
-                        icon: '🔹',
-                        text: '头像组件第一次地址加载报错后，数据再次获取后无法重新加载',
-                        link: '/bugs/web/element-ui/el-avatar_not_update_after_refetching_the_data'
-                    },
-                    {
-                        icon: '🔹',
-                        text: 'el-form 自定义一个非输入选择的值的规则，但是无法生效的问题',
-                        link: '/bugs/web/element-ui/el-form_customize_rule'
-                    }
-                ]
-            },
-            {
-                icon: uniAppIcon,
-                text: 'uni-app',
-                collapsed: true,
-                items: [
-                    {
-                        icon: '🔹',
-                        text: '新安装包覆盖安装，新增加的页面无法跳转',
-                        link: '/bugs/web/uniapp/overwrite_install_newly_added_page_cannot_jump'
-                    }
-                ]
-            },
-            {
-                icon: tauriIcon,
-                text: 'tauri',
-                collapsed: true,
-                items: [
-                    {
-                        icon: '🔹',
-                        text: '解决无法安装 Wix 和 Nsis 的问题',
-                        link: '/bugs/web/tauri/fix_tauri_cant_install_wix_and_nsis'
-                    }
-                ]
-            }
-        ]
+        items: web
     },
     {
         icon: editorIcon,
@@ -127,6 +32,17 @@ const bugsSidebar: SidebarType[] = [
                 text: 'Android Studio',
                 collapsed: true,
                 items: androidStudioItems
+            }
+        ]
+    },
+    {
+        text: '其他',
+        collapsed: true,
+        items: [
+            {
+                text: 'Windows',
+                collapsed: true,
+                items: windowsItems
             }
         ]
     }
---
+++ src/bugs/windows/win11_hotkey_conflict_fix.md
@@ -0,0 +1,27 @@
+---
+lastUpdated: 2025/09/21 17:59
+---
+
+# Windows11 某些快捷键（Ctrl+Shift+字母）被占用的解决方式
+
+::: tip 相关问题
+
+IDEA 快捷键 `Ctrl+Shift+X` 被占用
+
+:::
+
+## 解决思路
+
+### 1. 已启用软件排查
+
+这是个笨方法：逐个排查已启用的软件，查看该软件中的快捷键设置，看看有没有占用的。
+
+### 2. AMD Software（极有可能）
+
+如果你是 AMD 用户，且安装了 AMD Software（以前叫 Adrenalin），那么你可以搜索并打开它。
+
+![](https://namichong.obs.cn-south-1.myhuaweicloud.com/Blog/images/amd_software.png)
+
+然后点击右上角的齿轮图标，进入设置，禁用掉热键：
+
+![](https://namichong.obs.cn-south-1.myhuaweicloud.com/Blog/images/amd_software_disable_hotkey.png){width="75%"}
